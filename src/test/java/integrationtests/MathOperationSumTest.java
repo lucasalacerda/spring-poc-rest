68eb025decb4f2aab632a82c86f0cfc1211a31a6
Lucas <lucasalvlacerda@gmail.com>
2019-03-24 23:53:41 -0300

    add a example test using mockito

---
+++ src/test/java/integrationtests/MathOperationSumTest.java
@@ -0,0 +1,26 @@
+package integrationtests;
+
+import hello.MathOperation;
+import hello.MathOperationController;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+public class MathOperationSumTest {
+
+    @Test
+    public void test_when_sum_mockito_01(){
+        MathOperationController mathOperationController = Mockito.mock(MathOperationController.class);
+        Mockito.when(mathOperationController.sum(2, 2)).thenReturn("test mockito return");
+
+        String resultOfMath = mathOperationController.sum(2, 2);
+
+        Assertions.assertEquals("test mockito return", resultOfMath);
+    }
+
+}
